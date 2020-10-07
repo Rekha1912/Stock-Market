d3f612c4a9e013567ad09b759c2519bd31d4ff69
Rekha1912 <rekha.0019s@gmail.com>
2020-10-06 20:03:43 -0500

    api added

--- stocks/views.py
+++ stocks/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/AAPL/quote?token=pk_eb2cdb77af5847bb94c4ec9b9dfc076e")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "ERROR..."
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
