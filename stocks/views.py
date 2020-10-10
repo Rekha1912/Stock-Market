1dd5e7d71c088621d4d1a8a728288a786ca9469d
Rekha1912 <rekha.0019s@gmail.com>
2020-10-10 02:03:09 -0500

    function added

--- stocks/views.py
+++ stocks/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from .models import Userstock
 
 def home(request):
     import requests
     import json
     
-
     api_request = requests.get("https://cloud.iexapis.com/stable/stock/AAPL/quote?token=pk_eb2cdb77af5847bb94c4ec9b9dfc076e")
     try:
         api = json.loads(api_request.content)
@@ -14,12 +14,9 @@ def home(request):
     api_request1 = requests.get("https://cloud.iexapis.com/stable/stock/GOOG/quote?token=pk_eb2cdb77af5847bb94c4ec9b9dfc076e")
     try:
         api1 = json.loads(api_request1.content)
-        xxx = api1['latestPrice'] * 100
     except Exception as e:
         api1 = "ERROR..."
-    return render(request, 'home.html', {'api':api,'api1':api1, 'xxx':xxx})
-
-    
+    return render(request, 'home.html', {'api':api,'api1':api1})
 
 def search(request):
     import requests
@@ -37,4 +34,25 @@ def search(request):
         return render(request, 'search.html', {'search':"Enter symbol above...."})
 
 def user(request):
-    return render(request, 'user.html', {})
+    
+    import requests
+    import json
+    
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/AAPL/quote?token=pk_eb2cdb77af5847bb94c4ec9b9dfc076e")
+    try:
+        api = json.loads(api_request.content)
+        latvalue = api['latestPrice'] * 100
+    except Exception as e:
+        api = "ERROR..."
+
+    api_request1 = requests.get("https://cloud.iexapis.com/stable/stock/GOOG/quote?token=pk_eb2cdb77af5847bb94c4ec9b9dfc076e")
+    try:
+        api1 = json.loads(api_request1.content)
+        latvalue1 = api1['latestPrice'] * 100
+    except Exception as e:
+        api1 = "ERROR..."
+    
+   # total = api['latestPrice'] api1['latestPrice']
+
+    addstock = Userstock.objects.all()
+    return render(request, 'user.html', {'api':api,'api1':api1, 'latvalue':latvalue,'latvalue1':latvalue1,'addstock':addstock,})
