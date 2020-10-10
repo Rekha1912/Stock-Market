a2fb5ccf26a5bc57b4613cc7d08c34660f74ed84
Rekha1912 <rekha.0019s@gmail.com>
2020-10-10 02:01:59 -0500

    added model

--- stocks/models.py
+++ stocks/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class Userstock(models.Model):
+    stockname = models.CharField(max_length=20)
+    latestprice = models.IntegerField()
+    quantity = models.IntegerField()
+    value = models.IntegerField()
+    STOCK_ACTION = [
+        ('buy', 'BUY'),
+        ('sell', 'SELL'),
+    ]
+    action = models.CharField(
+        max_length=30,
+        choices=STOCK_ACTION,
+        default='BUY',
+    )
+      
+    def __str__(self):
+        return self.stockname
