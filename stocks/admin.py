0399f1c97485f65af0909038b69b6c6f3705aa92
Rekha1912 <rekha.0019s@gmail.com>
2020-10-11 18:15:32 -0500

    added new model

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Userstock
+from .models import Userstock , Cash, StockQuantity
 
 admin.site.register(Userstock)
+admin.site.register(Cash)
+admin.site.register(StockQuantity)
